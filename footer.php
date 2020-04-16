76250e8aa3a2cf81aa5bd4eb580db1e32cc903ca
Maharaj Fawwaz Almuqaddim Yusran <fawwazyusran@gmail.com>
2020-04-16 16:00:17 +0800

    Add header and footer template, Add navbar, Add home page (#5)

---
+++ footer.php
@@ -0,0 +1,3 @@
+<script src="https://cdn.metroui.org.ua/v4/js/metro.min.js"></script>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,11 @@
+<div class="fg-white bg-indigo" data-role="appbar" data-expand-point="md">
+  <a href="index.php" class="brand">
+    <b>Student Club Organizer</b>
+  </a>
+
+  <ul class="app-bar-menu ml-auto">
+    <li><a href="#">Login</a></li>
+    <li><a href="#">Sign Up</a></li>
+  </ul>
+</div>
+<div class="margin-navbar"></div>
