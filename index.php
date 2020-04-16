97edfdd8d0595a32282b2de631ee22885832dab4
Maharaj Fawwaz Almuqaddim Yusran <fawwazyusran@gmail.com>
2020-04-16 15:55:18 +0800

    Add header and footer template Add navbar Add home page

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+$pageTitle = 'Home';
+include "header.php";
+include "navbar.php";
+?>
+<div class="d-flex flex-align-center flex-justify-center flex-column w-100 home-wide-display">
+  <h1 class="text-center text-bold">Student Club Organizer</h1>
+  <h3 class="text-center fg-dark">A powerful tool to manage your organization</h3>
+</div>
+<div class="bg-darkIndigo fg-white p-6">
+  <div class="container">
+    <h1 class="text-bold">About Us</h1>
+    <p>The quick brown fox jumps over the lazy dog.</p>
+  </div>
+</div>
+<?php include "footer.php"; ?>
