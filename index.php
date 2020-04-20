cec13fb1c54ff4e628b7aa005519259b67527446
howwei98 <63303653+howwei98@users.noreply.github.com>
2020-04-20 16:36:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,52 @@ include "navbar.php";
 <div class="bg-darkIndigo fg-white p-6">
   <div class="container">
     <h1 class="text-bold">About Us</h1>
-    <p>The quick brown fox jumps over the lazy dog.</p>
+    <style>
+      p{
+        text-align:justify;
+        font-family: SansSerif;
+      }
+      td{
+        padding: 10px;
+      }
+    </style>
+    <p style="font-size: 20px">
+      We would like to provide a system that allow Universiti Malaysia Terengganu (UMT)
+      clubs to create events. The origin of the idea on building our system is to help
+      club representative build an event planning based on their club activity. The objective
+      are as below :
+      <ol>
+        <li>
+          To analyze on events occurred in UMT created and organize by club’s person in charge.
+        </li>
+        <li>
+          To design a perfect and efficient system that enables club to create an event that
+          satisfies their exigency.
+        </li>
+        <li>
+          To implement an easy approach of organizing event that supports the clubs’
+          vision and mission.
+        </li>
+      </ol>
+    </p>
+    <table>
+      <tr>
+        <td>
+          <h2>Mission</h2>
+          <p>
+            <i>To end the contemporary unsystematic event organizing system and create
+              a convenient and powerful event organizing system for user.</i>
+          </p>
+        </td>
+        <td>
+          <h2>Vision</h2>
+          <p>
+            <i>To create a user-friendly system which helps user to manage thier event
+              well. Our system could also notify user on the scheduled time.</i>
+          </p>
+        </td>
+      </tr>
+    </table>
   </div>
 </div>
 <?php include "footer.php"; ?>
